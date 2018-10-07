1e7211c64b2080b76b861c841263576dfcb264da
TrotzigerEmil <roadto3kmmr@gmail.com>
2018-10-07 14:42:46 +0700

    Initial commit

---
+++ include/stack.h
@@ -0,0 +1,34 @@
+#ifndef LONGCALC_H_STACK
+#define LONGCALC_H_STACK
+
+/* В заголовочном файле stack.h определена структура данных типа "стек", хранящая в
+   качестве данных строки символов, а также функции, производящие операции над сте-
+   ком (инициализация, добавление на вершину, удаление с вершины, удаление стека). */
+
+#define SUCCESS 0x00
+#define ERROR 0x01  /* выдать ошибку при заполнении  */
+#define EXPAND 0x02 /* расширить стек при заполнении */
+
+#define BSIZE 4096
+
+typedef struct stack Stack;
+
+struct stack *mallocStack(size_t, size_t, int, void (*)(struct stack *));
+
+int push(struct stack *, void *);
+
+int pop(struct stack *, void *);
+
+const void *const peek(struct stack *);
+
+int emptyp(struct stack *);
+
+size_t getsize(struct stack *);
+
+void printStack(struct stack *);
+
+void freeStack(struct stack *);
+
+void genericPrintFun(struct stack *);
+
+#endif
